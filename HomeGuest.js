f6f8c395704f82cf556649441dc8d100e86dc047
hawkesj12 <hawkesj12@gmail.com>
2020-08-26 14:01:39 -0400

    completed signup validation

--- HomeGuest.js
+++ HomeGuest.js
@@ -14,21 +14,21 @@ function HomeGuest() {
       hasErrors: false,
       message: "",
       isUnique: false,
-      checkCount: 0
+      checkCount: 0,
     },
     email: {
       value: "",
       hasErrors: false,
       message: "",
       isUnique: false,
-      checkCount: 0
+      checkCount: 0,
     },
     password: {
       value: "",
       hasErrors: false,
-      message: ""
+      message: "",
     },
-    submitCount: 0
+    submitCount: 0,
   }
 
   function ourReducer(draft, action) {
@@ -191,42 +191,42 @@ function HomeGuest() {
   }
 
   return (
-    <Page title='Welcome!' wide={true}>
-      <div className='row align-items-center'>
-        <div className='col-lg-7 py-3 py-md-5'>
-          <h1 className='display-3'>Remember Writing?</h1>
-          <p className='lead text-muted'>Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
+    <Page title="Welcome!" wide={true}>
+      <div className="row align-items-center">
+        <div className="col-lg-7 py-3 py-md-5">
+          <h1 className="display-3">Remember Writing?</h1>
+          <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
-        <div className='col-lg-5 pl-lg-5 pb-3 py-lg-5'>
+        <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
           <form onSubmit={handleSubmit}>
-            <div className='form-group'>
-              <label htmlFor='username-register' className='text-muted mb-1'>
+            <div className="form-group">
+              <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
               </label>
-              <input onChange={e => dispatch({ type: "usernameImmediately", value: e.target.value })} id='username-register' name='username' className='form-control' type='text' placeholder='Pick a username' autoComplete='off' />
-              <CSSTransition in={state.username.hasErrors} timeout={330} classNames='liveValidateMessage' unmountOnExit>
-                <div className='alert alert-danger small liveValidateMessage'>{state.username.message}</div>
+              <input onChange={(e) => dispatch({ type: "usernameImmediately", value: e.target.value })} id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
+              <CSSTransition in={state.username.hasErrors} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+                <div className="alert alert-danger small liveValidateMessage">{state.username.message}</div>
               </CSSTransition>
             </div>
-            <div className='form-group'>
-              <label htmlFor='email-register' className='text-muted mb-1'>
+            <div className="form-group">
+              <label htmlFor="email-register" className="text-muted mb-1">
                 <small>Email</small>
               </label>
-              <input onChange={e => dispatch({ type: "emailImmediately", value: e.target.value })} id='email-register' name='email' className='form-control' type='text' placeholder='you@example.com' autoComplete='off' />
-              <CSSTransition in={state.email.hasErrors} timeout={330} classNames='liveValidateMessage' unmountOnExit>
-                <div className='alert alert-danger small liveValidateMessage'>{state.email.message}</div>
+              <input onChange={(e) => dispatch({ type: "emailImmediately", value: e.target.value })} id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
+              <CSSTransition in={state.email.hasErrors} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+                <div className="alert alert-danger small liveValidateMessage">{state.email.message}</div>
               </CSSTransition>
             </div>
-            <div className='form-group'>
-              <label htmlFor='password-register' className='text-muted mb-1'>
+            <div className="form-group">
+              <label htmlFor="password-register" className="text-muted mb-1">
                 <small>Password</small>
               </label>
-              <input onChange={e => dispatch({ type: "passwordImmediately", value: e.target.value })} id='password-register' name='password' className='form-control' type='password' placeholder='Create a password' />
-              <CSSTransition in={state.password.hasErrors} timeout={330} classNames='liveValidateMessage' unmountOnExit>
-                <div className='alert alert-danger small liveValidateMessage'>{state.password.message}</div>
+              <input onChange={(e) => dispatch({ type: "passwordImmediately", value: e.target.value })} id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
+              <CSSTransition in={state.password.hasErrors} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+                <div className="alert alert-danger small liveValidateMessage">{state.password.message}</div>
               </CSSTransition>
             </div>
-            <button type='submit' className='py-3 mt-4 btn btn-lg btn-success btn-block'>
+            <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
               Sign up for ComplexApp
             </button>
           </form>
