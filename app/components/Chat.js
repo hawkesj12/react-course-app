85423148c8b3f9daf8e6d67f8c5fa677a425d30e
hawkesj12 <hawkesj12@gmail.com>
2020-08-27 14:20:24 -0400

    add backend address and netlify.toml

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -4,7 +4,7 @@ import DispatchContext from "../DispatchContext"
 import { useImmer } from "use-immer"
 import { Link } from "react-router-dom"
 import io from "socket.io-client"
-const socket = io("http://localhost:8080")
+const socket = io(process.env.BACKENDURL || "https://hawkesj12-backendforreactapp.herokuapp.com")
 
 function Chat() {
   const chatField = useRef(null)
