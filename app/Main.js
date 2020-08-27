9cc454fdfa1b90693d9afaba0b32f6cebcbe7591
hawkesj12 <hawkesj12@gmail.com>
2020-08-27 11:19:21 -0400

    added webpack dist task

--- app/Main.js
+++ app/Main.js
@@ -1,23 +1,24 @@
-import React, { useState, useReducer, useEffect } from "react"
+import React, { useState, useReducer, useEffect, Suspense } from "react"
 import ReactDOM from "react-dom"
 import { useImmerReducer } from "use-immer"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import { CSSTransition } from "react-transition-group"
 import Axios from "axios"
-Axios.defaults.baseURL = "http://localhost:8080"
+Axios.defaults.baseURL = process.env.BACKENDURL || ""
 
 import StateContext from "./StateContext"
 import DispatchContext from "./DispatchContext"
 
 // My Components
+import LoadingDotsIcon from "./components/LoadingDotsIcon"
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
 import Home from "./components/Home"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
-import CreatePost from "./components/CreatePost"
-import ViewSinglePost from "./components/ViewSinglePost"
+const CreatePost = React.lazy(() => import("./components/CreatePost"))
+const ViewSinglePost = React.lazy(() => import("./components/ViewSinglePost"))
 import FlashMessages from "./components/FlashMessages"
 import Profile from "./components/Profile"
 import EditPost from "./components/EditPost"
@@ -112,32 +113,34 @@ function Main() {
         <BrowserRouter>
           <FlashMessages messages={state.flashMessages} />
           <Header />
-          <Switch>
-            <Route path='/' exact>
-              {state.loggedIn ? <Home /> : <HomeGuest />}
-            </Route>
-            <Route path='/post/:id' exact>
-              <ViewSinglePost />
-            </Route>
-            <Route path='/post/:id/edit' excat>
-              <EditPost />
-            </Route>
-            <Route path='/profile/:username'>
-              <Profile />
-            </Route>
-            <Route path='/create-post'>
-              <CreatePost />
-            </Route>
-            <Route path='/about-us'>
-              <About />
-            </Route>
-            <Route path='/terms'>
-              <Terms />
-            </Route>
-            <Route>
-              <NotFound />
-            </Route>
-          </Switch>
+          <Suspense fallback={<LoadingDotsIcon />}>
+            <Switch>
+              <Route path='/' exact>
+                {state.loggedIn ? <Home /> : <HomeGuest />}
+              </Route>
+              <Route path='/post/:id' exact>
+                <ViewSinglePost />
+              </Route>
+              <Route path='/post/:id/edit' excat>
+                <EditPost />
+              </Route>
+              <Route path='/profile/:username'>
+                <Profile />
+              </Route>
+              <Route path='/create-post'>
+                <CreatePost />
+              </Route>
+              <Route path='/about-us'>
+                <About />
+              </Route>
+              <Route path='/terms'>
+                <Terms />
+              </Route>
+              <Route>
+                <NotFound />
+              </Route>
+            </Switch>
+          </Suspense>
           <CSSTransition timeout={330} in={state.isSearchOpen} classNames='search-overlay' unmountOnExit>
             <Search />
           </CSSTransition>
